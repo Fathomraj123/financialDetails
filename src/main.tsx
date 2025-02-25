71599a98317c546e34c03f9fd0a7bb1bb2cb5afe
revan <revanescence33@gmail.com>
2025-02-25 16:56:25 +0530

    Optimized

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import theme from "./theme";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider >
       <BrowserRouter>
         <App />
       </BrowserRouter>
