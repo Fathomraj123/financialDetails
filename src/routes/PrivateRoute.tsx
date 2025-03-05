f1e184c296b735c8cfaed74e4c2b14b384df468c
Fathomraj123 <sudarshankumarraj123@gmail.com>
2025-03-05 19:21:07 +0530

    PrivateRouteProps interface file

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,9 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { ReactNode } from "react";
 
-interface PrivateRouteProps {
-    children: ReactNode;
-}
+
 
 const PrivateRoute = ({ children }: PrivateRouteProps) => {
     const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
